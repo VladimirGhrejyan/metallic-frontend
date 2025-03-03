59da100440ebb777d4d70fbfef593289c3ecd7e5
suren <suren.stepanyan@polymorphic.io>
2025-03-03 14:53:33 +0400

    fix(m-1): fixed sign-in form password validation and error message

--- src/features/sign-in/model/form.constants.ts
+++ src/features/sign-in/model/form.constants.ts
@@ -8,6 +8,6 @@ export const signInFormConstants = {
         EMAIL_REQUIRED: 'Email is required',
         EMAIL_INVALID: 'Invalid email format',
 
-        PASSWORD_MIN: 'Password must be at least 6 characters long',
+        PASSWORD_REQUIRED: 'Password is required',
     },
 };
--- src/features/sign-in/model/form.schema.ts
+++ src/features/sign-in/model/form.schema.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 import { signInFormConstants } from './form.constants';
 
-const { EMAIL_REQUIRED, EMAIL_INVALID, PASSWORD_MIN } = signInFormConstants.ERRORS;
+const { EMAIL_REQUIRED, EMAIL_INVALID, PASSWORD_REQUIRED } = signInFormConstants.ERRORS;
 
 export const signInSchema = z.object({
     email: z.string().min(1, EMAIL_REQUIRED).email(EMAIL_INVALID),
-    password: z.string().min(6, PASSWORD_MIN),
+    password: z.string().min(1, PASSWORD_REQUIRED),
 });
