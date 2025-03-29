c34d4d9597fef9a3da13c4ffb9ac7436bd2d04c5
suren <suren.stepanyan@polymorphic.io>
2025-03-29 11:56:46 +0400

    refactor(m-1): commented out the necessary codes to restrict access home/sign-up pages

--- src/app/providers/router/config/routes/sign-up.route.ts
+++ src/app/providers/router/config/routes/sign-up.route.ts
@@ -1,9 +1,19 @@
-import { createRoute, lazyRouteComponent } from '@tanstack/react-router';
+import { createRoute, lazyRouteComponent, redirect } from '@tanstack/react-router';
 
 import { unauthenticatedLayoutRoute } from '../unauthenticated.route';
+
+// Temporary solution to restrict access to **SIGN UP (/sign-up)** page
+const condition = true;
 
 export const signUpRoute = createRoute({
     getParentRoute: () => unauthenticatedLayoutRoute,
     path: '/sign-up',
     component: lazyRouteComponent(() => import('~pages/sign-up'), 'SignUpPage'),
+    beforeLoad: () => {
+        if (condition) {
+            throw redirect({
+                to: '/sign-in',
+            });
+        }
+    },
 });
--- src/widgets/navbar/model/nav-items.tsx
+++ src/widgets/navbar/model/nav-items.tsx
@@ -1,11 +1,11 @@
 import { INavItem } from './nav-items.type';
 
 export const navItems: INavItem[] = [
-    {
-        name: 'Home',
-        to: '/',
-        icon: null,
-    },
+    // {
+    //     name: 'Home',
+    //     to: '/',
+    //     icon: null,
+    // }, // Temporary solution to restrict access to **HOME (/) ** page
     {
         name: 'Products',
         to: '/admin/products',
