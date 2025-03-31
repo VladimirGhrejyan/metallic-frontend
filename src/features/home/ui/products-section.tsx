90cac4562775b6d180c15a36f20600de107c6df6
suren <suren.stepanyan@polymorphic.io>
2025-03-31 21:37:00 +0400

    refactor(m-1): refactored and created necessary UI for home page

--- src/features/home/ui/products-section.tsx
+++ src/features/home/ui/products-section.tsx
@@ -1,81 +1,18 @@
 import { Box, Grid2 } from '@mui/material';
-import { ProductsCard } from '~shared/ui/componets';
+import { GetProductsApiResponse } from '~entities/product';
+import { ProductCard } from '~shared/ui/componets';
 
-interface IProduct {
-    code: string;
-    title: string;
-    costPrice: number;
-    markup: number;
-    categoryId: number;
-    imageUrl?: string;
+interface IProps {
+    data: GetProductsApiResponse['items'];
 }
 
-const rows: IProduct[] = [
-    {
-        code: 'iS483DUYJTa_AYC',
-        title: 'Product 1',
-        costPrice: 100,
-        markup: 200,
-        categoryId: 1,
-        imageUrl: 'https://placehold.co/600x400',
-    },
-    {
-        code: '3P8DCNFv8L8VjUb',
-        title: 'Product 2',
-        costPrice: 100,
-        markup: 200,
-        categoryId: 2,
-        imageUrl: 'https://placehold.co/600x400',
-    },
-    {
-        code: 'QKXH9A1E8OQcAWd',
-        title: 'Product 3',
-        costPrice: 100,
-        markup: 200,
-        categoryId: 2,
-        imageUrl: 'https://placehold.co/600x400',
-    },
-    {
-        code: 'R6qnvEPv_h0dhfZ',
-        title: 'Product 4',
-        costPrice: 100,
-        markup: 200,
-        categoryId: 3,
-        imageUrl: 'https://placehold.co/600x400',
-    },
-    {
-        code: '2MYaS7ng0oIBMb1',
-        title: 'Product 5',
-        costPrice: 100,
-        markup: 200,
-        categoryId: 4,
-        imageUrl: 'https://placehold.co/600x400',
-    },
-    {
-        code: '3Ct__c6a0zWqqgX',
-        title: 'Product 6',
-        costPrice: 100,
-        markup: 200,
-        categoryId: 4,
-        imageUrl: 'https://placehold.co/600x400',
-    },
-    {
-        code: 'rWCavsEbHP28psp',
-        title: 'Product 7',
-        costPrice: 100,
-        markup: 200,
-        categoryId: 4,
-        imageUrl: 'https://placehold.co/600x400',
-    },
-];
-
-export const ProductsSection = () => {
+export const ProductsSection = ({ data }: IProps) => {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Grid2 container spacing={4}>
-                {rows.map((product) => (
+                {data.map((product) => (
                     <Grid2 key={product.code}>
-                        <ProductsCard product={product} />
+                        <ProductCard product={product} />
                     </Grid2>
                 ))}
             </Grid2>
--- src/pages/home/ui/home.page.tsx
+++ src/pages/home/ui/home.page.tsx
@@ -1,21 +1,119 @@
-import { Box } from '@mui/material';
-import { useCallback, useState } from 'react';
+import { Box, TablePagination } from '@mui/material';
+import { ChangeEvent, MouseEvent, useCallback, useMemo, useState } from 'react';
+import { useGetProductsQuery } from '~entities/product';
+import { ProductsFilters } from '~features/admin-products';
 import { ProductsSection } from '~features/home';
-import { PageHeader } from '~shared/ui/componets';
-import { Search } from '~widgets/search';
+import { TProductsQueryArgs } from '~pages/admin-products/model/get-products/admin-products.types';
+import { Loader, PageHeader } from '~shared/ui/componets';
+import { SearchInput } from '~shared/ui/componets/search-input';
+import { FiltersPopover } from '~widgets/filters-popover';
+
+const filterQueryArgs = (
+    queryArgs: TProductsQueryArgs,
+    incrementPage: boolean = false,
+): Omit<TProductsQueryArgs, ''> => {
+    return {
+        ...Object.fromEntries(
+            Object.entries(queryArgs).filter(([_, value]) => value !== undefined && value !== ''),
+        ),
+        ...(incrementPage && queryArgs.page
+            ? { page: (Number(queryArgs.page) + 1).toString() }
+            : {}),
+    };
+};
 
 export const HomePage = () => {
-    const [searchValue, setSearchValue] = useState<string>('');
+    const [queryArgs, setQueryArgs] = useState<TProductsQueryArgs>({
+        page: '0',
+        itemsPerPage: '10',
+    });
 
-    const handleSearchChange = useCallback((value: string) => {
-        setSearchValue(value);
-    }, []);
+    const { data } = useGetProductsQuery({
+        ...filterQueryArgs(queryArgs, true),
+    });
+
+    const onSearch = useCallback(
+        (value: string) => {
+            setQueryArgs((prev) => ({ ...prev, search: value, page: '0' }));
+        },
+        [setQueryArgs],
+    );
+
+    const onPageChange = useCallback(
+        (
+            _: MouseEvent<HTMLElement> | MouseEvent<HTMLButtonElement, MouseEvent> | null,
+            newPage: number,
+        ) => {
+            setQueryArgs((prev) => ({ ...prev, page: String(newPage) }));
+        },
+        [setQueryArgs],
+    );
+
+    const onRowsPerPageChange = useCallback(
+        (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | undefined) => {
+            setQueryArgs((prev) => ({ ...prev, itemsPerPage: event?.target.value, page: '0' }));
+        },
+        [setQueryArgs],
+    );
+
+    const onFiltersSubmit = useCallback(
+        (values: Pick<TProductsQueryArgs, 'categoryId' | 'sortBy' | 'order'>) => {
+            setQueryArgs((prev) => ({ ...prev, ...values }));
+        },
+        [setQueryArgs],
+    );
+
+    const onResetFilters = useCallback(() => {
+        setQueryArgs((prev) => {
+            const { categoryId, order, sortBy, ...rest } = prev;
+            return rest;
+        });
+    }, [setQueryArgs]);
+
+    const memoizedDefaultFilters: Omit<TProductsQueryArgs, 'itemsPerPage' | 'page' | 'search'> =
+        useMemo(() => {
+            const { itemsPerPage, page, search, ...rest } = filterQueryArgs(queryArgs);
+            return rest;
+        }, [queryArgs]);
+
+    if (!data) {
+        return <Loader />;
+    }
 
     return (
-        <Box display="flex" flexDirection="column" gap={2}>
+        <Box
+            display="flex"
+            height="100%"
+            flexDirection="column"
+            justifyContent="space-between"
+            position="relative"
+            gap={2}
+        >
             <PageHeader pageTitle="Dashboard" />
-            <Search searchValue={searchValue} onChange={handleSearchChange} />
-            <ProductsSection />
+            <Box display="flex" gap={2}>
+                <SearchInput onSearch={onSearch} />
+                <FiltersPopover>
+                    <ProductsFilters
+                        defaultValues={memoizedDefaultFilters}
+                        onFiltersSubmit={onFiltersSubmit}
+                        onResetFilters={onResetFilters}
+                    />
+                </FiltersPopover>
+            </Box>
+            <ProductsSection data={data.items} />
+            <TablePagination
+                count={data.meta.totalItems}
+                page={Number(queryArgs.page) || 0}
+                rowsPerPage={data.meta.itemsPerPage}
+                onPageChange={onPageChange}
+                rowsPerPageOptions={[5, 10, 25, 50, 100]}
+                onRowsPerPageChange={onRowsPerPageChange}
+                sx={{
+                    position: 'fixed',
+                    right: '16px',
+                    bottom: '16px',
+                }}
+            />
         </Box>
     );
 };
