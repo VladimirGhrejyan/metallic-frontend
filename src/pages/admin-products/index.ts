7d060951707bcd44dace6798f8504aaac68f5719
suren <suren.stepanyan@polymorphic.io>
2025-04-09 19:58:08 +0400

    refactor(m-1): fix typo

--- src/pages/admin-products/index.ts
+++ src/pages/admin-products/index.ts
@@ -1,3 +1,3 @@
-export { CreateProductPage } from './ui/create-product/create-product-page';
+export { CreateProductPage } from './ui/create-product/create-product.page';
 export { GetProductsPage } from './ui/get-products/get-products.page';
-export { UpdateProductPage } from './ui/update-product/update-product-page';
+export { UpdateProductPage } from './ui/update-product/update-product.page';
