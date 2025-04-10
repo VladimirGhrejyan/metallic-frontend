3e9275b3b3f817bbe770cadac505bf646aa66309
suren <suren.stepanyan@polymorphic.io>
2025-04-10 19:50:03 +0400

    feat(m-1): wrapped the product card component with a Link

--- src/shared/ui/componets/product-card.tsx
+++ src/shared/ui/componets/product-card.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
-import { useNavigate } from '@tanstack/react-router';
+import { Link, useNavigate } from '@tanstack/react-router';
 import React from 'react';
 import { GetProductByIdApiResponse } from '~entities/product';
 import { calculateTotalPrice } from '~shared/helpers';
@@ -12,54 +12,57 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     const navigate = useNavigate();
     const productImageUrl = product.image?.url || 'src/shared/assets/placeholder.png';
     return (
-        <Card
-            onClick={() => navigate({ to: `/admin/products/${product.id}/edit` })}
-            sx={{
-                width: 300,
-                height: 320,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                borderRadius: 2,
-                boxShadow: 2,
-                transition: 'transform 0.1s ease-in-out',
-                '&:hover': {
-                    transform: 'scale(1.01)',
-                    cursor: 'pointer',
-                },
-            }}
-        >
-            <CardMedia
-                component="img"
-                image={productImageUrl}
-                alt={product.title}
+        <Link to={`/admin/products/${product.id}/edit`} style={{ textDecoration: 'none' }}>
+            <Card
+                onClick={() => navigate({ to: `/admin/products/${product.id}/edit` })}
                 sx={{
-                    height: 180,
-                    width: '100%',
-                    objectFit: 'cover',
-                    backgroundColor: 'background.default',
+                    width: 300,
+                    height: 320,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    borderRadius: 2,
+                    boxShadow: 2,
+                    transition: 'transform 0.1s ease-in-out',
+                    '&:hover': {
+                        transform: 'scale(1.01)',
+                        cursor: 'pointer',
+                    },
+                    textDecoration: 'none',
                 }}
-            />
-            <CardContent sx={{ px: 0, py: 1 }}>
-                <Typography
-                    fontWeight="bold"
+            >
+                <CardMedia
+                    component="img"
+                    image={productImageUrl}
+                    alt={product.title}
                     sx={{
-                        overflow: 'hidden',
-                        display: '-webkit-box',
-                        WebkitLineClamp: 1,
-                        WebkitBoxOrient: 'vertical',
-                        textOverflow: 'ellipsis',
+                        height: 180,
+                        width: '100%',
+                        objectFit: 'cover',
+                        backgroundColor: 'background.default',
                     }}
-                >
-                    {product.title}
-                </Typography>
-                <Typography variant="body1" noWrap>
-                    Code: {product.code}
-                </Typography>
-                <Typography variant="body1">
-                    Total Price: {calculateTotalPrice(product.costPrice, product.markup)}
-                </Typography>
-            </CardContent>
-        </Card>
+                />
+                <CardContent sx={{ px: 0, py: 1 }}>
+                    <Typography
+                        fontWeight="bold"
+                        sx={{
+                            overflow: 'hidden',
+                            display: '-webkit-box',
+                            WebkitLineClamp: 1,
+                            WebkitBoxOrient: 'vertical',
+                            textOverflow: 'ellipsis',
+                        }}
+                    >
+                        {product.title}
+                    </Typography>
+                    <Typography variant="body1" noWrap>
+                        Code: {product.code}
+                    </Typography>
+                    <Typography variant="body1">
+                        Total Price: {calculateTotalPrice(product.costPrice, product.markup)}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Link>
     );
 };
