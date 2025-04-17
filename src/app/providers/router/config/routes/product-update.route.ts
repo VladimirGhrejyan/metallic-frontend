c6a36989f0be96e5fe7b3c517aa8ca59472fb263
suren <suren.stepanyan@polymorphic.io>
2025-04-17 22:35:59 +0400

    feat(): added search param referrer and used it for navigation

--- src/app/providers/router/config/routes/product-update.route.ts
+++ src/app/providers/router/config/routes/product-update.route.ts
@@ -2,8 +2,15 @@ import { createRoute, lazyRouteComponent } from '@tanstack/react-router';
 
 import { adminRoute } from './admin.route';
 
+interface ISearch {
+    referrer: 'view' | undefined;
+}
+
 export const productUpdateRoute = createRoute({
     getParentRoute: () => adminRoute,
     path: '/products/$productId/edit',
     component: lazyRouteComponent(() => import('~pages/admin-products'), 'UpdateProductPage'),
+    validateSearch: (search: ISearch): ISearch => {
+        return { ...search };
+    },
 });
