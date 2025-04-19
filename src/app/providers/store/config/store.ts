461a9f88eebab8d2672d458cb360cd55a09b7503
suren <suren.stepanyan@polymorphic.io>
2025-04-19 23:27:46 +0400

    feat(): created a snackbar slice and added it to the redux store

--- src/app/providers/store/config/store.ts
+++ src/app/providers/store/config/store.ts
@@ -2,13 +2,19 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { api } from '~shared/config/api';
 
 import { authMiddleware } from '../lib/auth.middleware';
+import { snackbarSlice } from '../lib/snackbar-slice';
+import { toastiMiddleware } from '../lib/toast.middleware';
 
 const rootReducer = combineReducers({
     [api.reducerPath]: api.reducer,
+    snackbar: snackbarSlice.reducer,
 });
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat([api.middleware, authMiddleware]),
+        getDefaultMiddleware().concat([api.middleware, authMiddleware, toastiMiddleware]),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
