42f2fe1cf5b09bf426f57c7177c5043ad6be1ec1
VladimirGhrejyan <vladimir.ghrejyan@gmail.com>
2026-03-06 20:54:48 +0400

    refactor(architecture-enhancements): update eslint, typescript and plugin packages versions

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,8 +3,6 @@ import type { UserConfig } from '@commitlint/types';
 const Configuration: UserConfig = {
     extends: ['@commitlint/config-conventional'],
 
-    parserPreset: 'conventional-changelog-atom',
-
     formatter: '@commitlint/format',
 
     rules: {
